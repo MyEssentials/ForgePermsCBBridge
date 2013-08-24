857aafb2bc5de147dee945bc5a3d9492db84d527
Torins <shoeth@gmail.com>
2013-08-24 02:31:19 +0200

    Added config option for debug logging. Logging disabled by default

--- src/com/sperion/forgeperms/cbbridge/Bridge.java
+++ src/com/sperion/forgeperms/cbbridge/Bridge.java
@@ -4,32 +4,43 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.bukkit.Server;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sperion.forgeperms.ForgePerms;
 
 public class Bridge extends JavaPlugin {
-    public static Logger log;
-    public static Server server;
-    public static Bridge instance;
-    
-    @Override
-    public void onLoad() {
-        instance = this;
-        log = Logger.getLogger("Minecraft");
-        server = getServer();
-    }
-    
-    public static void log(Level level, String msg){
-        log.log(level, "[Bridge]" + msg);
-    }
-    
-    @Override
-    public void onDisable() {
-    }
+	public static Logger log;
+	public static Server server;
+	public static Bridge instance;
 
-    @Override
-    public void onEnable() {
-        ForgePerms.registerHandler(new VaultPermissions());
-    }
+	private Configuration config;
+	private static boolean debugLogEnabled;
+
+	@Override
+	public void onLoad() {
+		instance = this;
+		log = Logger.getLogger("Minecraft");
+		server = getServer();
+		config = getConfig();
+		debugLogEnabled = config.getBoolean("enableDebugLogging", false);
+		config.set("enableDebugLogging", debugLogEnabled);
+		saveConfig();
+	}
+
+	public static void log(Level level, String msg) {
+		if (debugLogEnabled) {
+			log.log(level, "[Bridge]" + msg);
+		}
+
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public void onEnable() {
+		ForgePerms.registerHandler(new VaultPermissions());
+	}
 }
